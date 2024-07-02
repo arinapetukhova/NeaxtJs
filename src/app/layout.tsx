e74a2d2db9e51bb75e354e9a285c861fa5a7d6c3
Arina Petuhova <119685834+arinapetukhova@users.noreply.github.com>
2024-07-02 16:03:07 +0300

    Route paths change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
                 </li>
                 <li>
                   <a
-                    href="/myportfolio"
+                    href="/NextJs/myportfolio"
                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-amber-400 md:p-0"
                   >
                     Portfolio
